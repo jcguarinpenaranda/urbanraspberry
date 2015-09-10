3a71a0680fc7e255f867cf0c1c006ebb358a2d2d
Juan Camilo Guarín Peñaranda <jcguarinpenaranda@gmail.com>
2015-09-10 10:48:56 -0500

    Comentarios en las rutas del webservice

--- index.php
+++ index.php
@@ -5,16 +5,48 @@ require 'Slim/Slim.php';
 
 $app = new \Slim\Slim();
 
+/*
+En /variables un cliente que haga
+un get obtendrá los datos de los pines
+en los cuales tendrá conectado uno
+o varios sensores, y el nombre de
+la variable que corresponde a cada
+uno.
 
-$app->get('/',function(){
-	echo "Hola";
+El formato propuesto para recibir es:
+
+{
+	"variable":"temperatura",
+	"pines":["a0","a1"],
+	"frecuencia":60 //en segundos
+}
+
+*/
+$app->get('/variables',function(){
+
+
+
 });
 
-$app->post('/',function() use($app){
-	$a = $app->request->params();
 
-	echo "Datos enviados:";
-	var_dump($a);
+/*
+En /datosensor, un cliente que haga
+una petición post podrá enviar datos
+directamente a la plataforma de
+UrbanEyes
+
+El formato que debe enviar el cliente
+será:
+
+{
+	"value":123,
+	"variable":"temperatura"
+}
+
+*/
+$app->post('/datosensor',function() use($app){
+	//$a = $app->request->params();
+
 });
 
 
