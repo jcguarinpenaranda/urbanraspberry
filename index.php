e66f5a53aa5c509625a6b12f1ed0f719c42e6efa
Juan Camilo Guarin P <jcguarinpenaranda@gmail.com>
2015-10-08 16:03:57 -0500

    Ya se pueden añadir dispositivos a la lista y se sincroizan desde la interfaz al webservice

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require 'Slim/Slim.php';
 $app = new \Slim\Slim();
 
 /*
-En /variables un cliente que haga
+En /equipos un cliente que haga
 un get obtendrá los datos de los pines
 en los cuales tendrá conectado uno
 o varios sensores, y el nombre de
@@ -19,15 +19,85 @@ uno.
 El formato propuesto para recibir es:
 
 {
+	"id":"abc",
+	"nombre":"Equipo prueba",
 	"variable":"temperatura",
 	"pines":["a0","a1"],
 	"frecuencia":60 //en segundos
 }
+*/
+$app->get('/equipos/',function(){
+	$texto = file_get_contents("equipos.json");
+	echo $texto;
+});
 
+
+/*
+En /equipos, un cliente que haga POST
+podrá añadir un nuevo equipo
+*/
+$app->post('/equipos/',function(){
+	$app = \Slim\Slim::getInstance();
+	$req = $app->request;
+	$params = $req->params();
+
+	//var_dump($params);
+
+	$texto = file_get_contents("equipos.json");
+	//var_dump($texto);
+
+	$equipos = json_decode($texto, true);	
+	//var_dump($equipos);
+
+	$existe = false;
+
+	for($i =0 ; $i<count($equipos); $i++){
+		//var_dump($equipos[$i]['id']);
+		//echo "Igual: ".$params['id']." ".$equipos[$i]['id'];
+		if(isset($equipos[$i]['id']) && isset($params['id'])){
+			if($equipos[$i]['id'] === $params['id']){
+				//echo $equipos[$i]['id'];
+				$existe = true;
+			}
+		}
+	}
+
+	if($existe == true){
+
+		$status = array();
+		$status['status']= 304;
+		$status['description'] = "El equipo ya existe";
+
+		echo json_encode($status);
+
+	}else{
+
+		$equipos[]=array("id"=>$params['id'], "nombre"=>$params['nombre'], "variables"=> array(), "frecuencia"=>null);
+		
+		//var_dump($equipos);
+
+
+		//Se escribe en el archivo el contenido del arreglo con la adición.
+		file_put_contents("equipos.json", json_encode($equipos));
+
+		$status = array(
+			"status"=>201,
+			"description" => "Equipo Creado");
+
+		echo json_encode($status);		
+	}
+
+});
+
+
+/*
+En /variables, un cliente que haga un 
+get, obtendrá la lista de variables
+disponibles para hacer reportes en el
+sistema de UrbanEyes.
 */
 $app->get('/variables/',function(){
-	$texto = file_get_contents("variables.json");
-	echo $texto;
+	$app = \Slim\Slim::getInstance();
 });
 
 
@@ -43,7 +113,7 @@ será:
 {
 	"value":123,
 	"variable_id":3
-00}
+}
 
 */
 $app->post('/datosensor/',function() use($app){
@@ -59,7 +129,8 @@ $app->post('/datosensor/',function() use($app){
     $a ["date"]= date('d-m-y_H:i:s');
     $a ["description"]= "urbanraspberry";
 
-//    $req = \Httpful\Request::post('http://181.118.150.147/sensor/create/', json_encode($a), "application/json");
+    //$req = \Httpful\Request::post('http://181.118.150.147/sensor/create/', json_encode($a), "application/json");
+    
 
     $headers= array("Accept" => "application/x-www-form-urlencoded");
 
